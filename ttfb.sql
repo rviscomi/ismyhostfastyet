76669c29204f2e7c1da9ed50a5ba46b3c0b27801
Rick Viscomi <rviscomi@users.noreply.github.com>
2022-08-09 18:08:37 -0400

    Update ttfb.sql

--- ttfb.sql
+++ ttfb.sql
@@ -5,13 +5,13 @@ WITH crux AS (
   FROM
     `chrome-ux-report.materialized.device_summary`
   WHERE
-    date = '2022-06-01'
+    date = '2022-07-01'
 ), requests AS (
   SELECT
     _TABLE_SUFFIX,
     *
   FROM
-    `httparchive.summary_requests.2022_06_01_*`
+    `httparchive.summary_requests.2022_07_01_*`
 )
 
 SELECT DISTINCT
