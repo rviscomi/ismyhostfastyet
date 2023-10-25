021b6094b63e552d368699685c1f83b8956a123b
Rick Viscomi <rviscomi@users.noreply.github.com>
2023-10-25 11:37:00 -0400

    Update ttfb.sql

--- ttfb.sql
+++ ttfb.sql
@@ -1,67 +1,43 @@
-# Update these two subqueries monthly.
-WITH crux AS (
-  SELECT
-    *
-  FROM
-    `chrome-ux-report.materialized.device_summary`
-  WHERE
-    date = '2023-09-01'
-), requests AS (
-  SELECT
-    client,
-    root_page,
-    JSON_VALUE(summary, '$.respOtherHeaders') AS respOtherHeaders,
-    JSON_VALUE(summary, '$.resp_x_powered_by') AS resp_x_powered_by,
-    JSON_VALUE(summary, '$.resp_via') AS resp_via,
-    JSON_VALUE(summary, '$.resp_server') AS resp_server
-  FROM
-    `httparchive.all.requests`
-  WHERE
-    date = '2023-09-01' AND
-    is_main_document
-)
+# Update this monthly.
+DECLARE QUERY_DATE DATE DEFAULT '2023-09-01';
 
-SELECT DISTINCT
-  CASE
-   WHEN platform = 'zyro.com' THEN 'Hostinger Website Builder'
-   WHEN platform = 'hostinger' THEN 'Hostinger'
-   WHEN platform = 'zoneos' THEN 'Zone.eu'
-   WHEN platform = 'seravo' THEN 'Seravo'
-   WHEN platform = 'automattic.com/work-with-us' THEN 'Automattic'
-   WHEN platform = 'wpvip.com/careers' THEN 'Automattic'
-   WHEN platform = 'wordpress.com' THEN 'Automattic'
-   WHEN platform = 'a9130478a60e5f9135f765b23f26593b' then 'Automattic'
-   WHEN platform = 'x-ah-environment' THEN 'Acquia'
-   WHEN platform = 'x-pantheon-styx-hostname' THEN 'Pantheon'
-   WHEN platform = 'wp engine atlas' THEN 'WP Engine Atlas'
-   WHEN platform = 'wpe-backend' THEN 'WP Engine'
-   WHEN platform = 'wp engine' THEN 'WP Engine'
-   WHEN platform = 'x-kinsta-cache' THEN 'Kinsta'
-   WHEN platform = 'hubspot' THEN 'HubSpot'
-   WHEN platform = 'b7440e60b07ee7b8044761568fab26e8' THEN 'SiteGround'
-   WHEN platform = '624d5be7be38418a3e2a818cc8b7029b' THEN 'SiteGround'
-   WHEN platform = '6b7412fb82ca5edfd0917e3957f05d89' THEN 'SiteGround'
-   WHEN platform = 'x-github-request' THEN 'GitHub'
-   WHEN platform = 'alproxy' THEN 'AlwaysData'
-   WHEN platform = 'netlify' THEN 'Netlify'
-   WHEN platform = 'x-lw-cache' THEN 'Liquid Web'
-   WHEN platform = 'squarespace' THEN 'Squarespace'
-   WHEN platform = 'x-wix-request-id' THEN 'Wix'
-   WHEN platform = 'x-shopify-stage' THEN 'Shopify'
-   WHEN platform = 'x-vercel-id' THEN 'Vercel'
-   WHEN platform = 'flywheel' THEN 'Flywheel'
-   WHEN platform = 'weebly' THEN 'Weebly'
-   WHEN platform = 'dps/' THEN 'GoDaddy Website Builder'
-   WHEN platform = 'awex' THEN '000webhost'
-   WHEN platform = 'wp-cloud' THEN 'WP-Cloud'
-   ELSE NULL
-  END AS platform,
-  client,
-  COUNT(DISTINCT url) AS n,
-  COUNT(DISTINCT IF(ttfb = 'Good', url, NULL)) / COUNT(DISTINCT url) AS fast,
-  COUNT(DISTINCT IF(ttfb = 'Needs Improvement', url, NULL)) / COUNT(DISTINCT url) AS avg,
-  COUNT(DISTINCT IF(ttfb = 'Poor', url, NULL)) / COUNT(DISTINCT url) AS slow
-FROM (
+# Add/edit platforms in alphabetical order here.
+DECLARE PLATFORMS ARRAY<STRUCT<regex STRING, name STRING>> DEFAULT [
+  ('awex', '000webhost'),
+  ('x-ah-environment', 'Acquia'),
+  ('alproxy', 'AlwaysData'),
+  ('automattic.com/work-with-us', 'Automattic'),
+  ('wpvip.com/careers', 'Automattic'),
+  ('wordpress.com', 'Automattic'),
+  ('a9130478a60e5f9135f765b23f26593b', 'Automattic'),
+  ('flywheel', 'Flywheel'),
+  ('x-github-request', 'GitHub'),
+  ('dps/', 'GoDaddy Website Builder'),
+  ('hostinger', 'Hostinger'),
+  ('zyro.com', 'Hostinger Website Builder'),
+  ('hubspot', 'HubSpot'),
+  ('x-kinsta-cache', 'Kinsta'),
+  ('x-lw-cache', 'Liquid Web'),
+  ('netlify', 'Netlify'),
+  ('x-pantheon-styx-hostname', 'Pantheon'),
+  ('seravo', 'Seravo'),
+  ('x-shopify-stage', 'Shopify'),
+  ('b7440e60b07ee7b8044761568fab26e8', 'SiteGround'),
+  ('624d5be7be38418a3e2a818cc8b7029b', 'SiteGround'),
+  ('6b7412fb82ca5edfd0917e3957f05d89', 'SiteGround'),
+  ('squarespace', 'Squarespace'),
+  ('x-vercel-id', 'Vercel'),
+  ('weebly', 'Weebly'),
+  ('x-wix-request-id', 'Wix'),
+  ('wp-cloud', 'WP-Cloud'),
+  ('wpe-backend', 'WP Engine'),
+  ('wp engine', 'WP Engine'),
+  ('wp engine atlas', 'WP Engine Atlas'),
+  ('zoneos', 'Zone.eu')
+];
+
+
+WITH crux AS (
   SELECT
     IF(device = 'desktop', 'desktop', 'mobile') AS client,
     CONCAT(origin, '/') AS url,
@@ -72,22 +48,67 @@ FROM (
       ELSE NULL
     END AS ttfb
   FROM
-    crux
+    `chrome-ux-report.materialized.device_summary`
   WHERE
+    date = QUERY_DATE AND
     device IN ('desktop', 'phone') AND
-    fast_ttfb IS NOT NULL)
-JOIN (
+    fast_ttfb IS NOT NULL
+),
+
+requests AS (
   SELECT
     client,
-    root_page AS url,
-    REGEXP_EXTRACT(LOWER(CONCAT(IFNULL(respOtherHeaders, ''), IFNULL(resp_x_powered_by, ''), IFNULL(resp_via, ''), IFNULL(resp_server, ''))),
-      r'(zoneos|seravo|x-kinsta-cache|automattic.com/work-with-us|wpvip.com/careers|wordpress\.com|a9130478a60e5f9135f765b23f26593b|x-ah-environment|x-pantheon-styx-hostname|wp engine atlas|wpe-backend|wp engine|hubspot|b7440e60b07ee7b8044761568fab26e8|624d5be7be38418a3e2a818cc8b7029b|6b7412fb82ca5edfd0917e3957f05d89|x-github-request|alproxy|netlify|x-lw-cache|squarespace|x-wix-request-id|x-shopify-stage|x-vercel-id|flywheel|weebly|dps/|hostinger|awex|wp-cloud|zyro.com)') AS platform
+    root_page,
+    JSON_VALUE(summary, '$.respOtherHeaders') AS respOtherHeaders,
+    JSON_VALUE(summary, '$.resp_x_powered_by') AS resp_x_powered_by,
+    JSON_VALUE(summary, '$.resp_via') AS resp_via,
+    JSON_VALUE(summary, '$.resp_server') AS resp_server
   FROM
-    requests)
+    `httparchive.all.requests`
+  WHERE
+    date = QUERY_DATE AND
+    is_main_document
+),
+
+platform_regex AS (
+  SELECT
+    STRING_AGG(regex, r'|') AS pattern
+  FROM
+    UNNEST(PLATFORMS)
+),
+
+detected_platforms AS (
+  SELECT
+    client,
+    url,
+    name AS platform
+  FROM
+    UNNEST(PLATFORMS)
+  JOIN (
+    SELECT
+      client,
+      root_page AS url,
+      REGEXP_EXTRACT(LOWER(CONCAT(IFNULL(respOtherHeaders, ''), IFNULL(resp_x_powered_by, ''), IFNULL(resp_via, ''), IFNULL(resp_server, ''))), (SELECT pattern FROM platform_regex)) AS regex
+    FROM
+      requests)
+  USING
+    (regex)
+)
+
+
+SELECT DISTINCT
+  platform,
+  client,
+  COUNT(DISTINCT url) AS n,
+  COUNT(DISTINCT IF(ttfb = 'Good', url, NULL)) / COUNT(DISTINCT url) AS fast,
+  COUNT(DISTINCT IF(ttfb = 'Needs Improvement', url, NULL)) / COUNT(DISTINCT url) AS avg,
+  COUNT(DISTINCT IF(ttfb = 'Poor', url, NULL)) / COUNT(DISTINCT url) AS slow
+FROM
+  detected_platforms
+JOIN
+  crux
 USING
   (client, url)
-WHERE
-  platform IS NOT NULL
 GROUP BY
   platform,
   client
